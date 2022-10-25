5edfb984c85520b2aeeb59508fa2116ddda9da74
raihan512 <raihangazi1024@Gmail.com>
2022-10-26 00:00:31 +0600

    generated link for single course item and send it

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
-
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course.id === id);
+    res.send(selectedCourse)
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
